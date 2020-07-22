7d2540135d998be8979284d54315db10d29456ec
Rick Cogley <rick.cogley@esolia.co.jp>
2020-07-22 18:35:45 +0900

    Initial, just testing
    Signed-off-by: Rick Cogley <rick.cogley@esolia.co.jp>

---
+++ PROdb/esolia-update-denphone-cdr-01.js
@@ -0,0 +1,8 @@
+// Call from github 
+// result = import('https://raw.githubusercontent.com/RickCogley/webhook.site/master/PROdb/esolia-update-denphone-cdr-01.js')
+// echo(result) 
+
+// Configuration
+
+denphone_token_esolia_01 = var('g_denphone_token_esolia_01');
+echo(denphone_token_esolia_01);
