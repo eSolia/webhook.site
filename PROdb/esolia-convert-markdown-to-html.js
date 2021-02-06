63d9f654f03c336f681ea3d3e07fa9f5bb8b0741
Rick Cogley <rick.cogley@esolia.co.jp>
2021-02-06 16:34:10 +0900

    Update esolia-convert-markdown-to-html.js

--- PROdb/esolia-convert-markdown-to-html.js
+++ PROdb/esolia-convert-markdown-to-html.js
@@ -1,3 +1,7 @@
 // Call from github 
 // result = import('https://raw.githubusercontent.com/RickCogley/webhook.site/master/PROdb/esolia-convert-markdown-to-html.js')
 // echo(result) 
+
+// Prep 
+md = markdown_to_html(var("request.content"));
+echo(md);
