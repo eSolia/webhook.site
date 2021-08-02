cb5b7b9152ec8db0fde97b9fcad28288b5a85483
Rick Cogley <rick.cogley@esolia.co.jp>
2021-08-02 06:15:53 +0000

    Clean up load csv scripts for MGJ

---
+++ MGJ/mgj-load-csv-01b.js
@@ -0,0 +1,3 @@
+// Call from github
+//result = import('https://raw.githubusercontent.com/RickCogley/webhook.site/master/PROdb/mgj-load-csv-01c.js')
+//echo(result)
---
+++ MGJ/mgj-load-csv-01c.js
@@ -0,0 +1,3 @@
+// Call from github
+//result = import('https://raw.githubusercontent.com/RickCogley/webhook.site/master/PROdb/mgj-load-csv-01b.js')
+//echo(result)
