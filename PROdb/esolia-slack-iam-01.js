904499bd971679b495df4ed127b8a54642d6619c
Rick Cogley <rick.cogley@esolia.co.jp>
2021-08-13 09:57:15 +0000

    Cleanup of extraneous vars and comments

--- PROdb/esolia-slack-iam-01.js
+++ PROdb/esolia-slack-iam-01.js
@@ -10,25 +10,10 @@ prodb_token = var('g_prodb_token');
 slack_signsecret = var('g_slack_signsecret_001');
 prodb_status_create_url = var('g_prodb_status_create_url');
 
-echo("SLACK PAYLOAD FORM and HEADER PARAMS");
+echo("SLACK PAYLOAD and HEADER PARAMS for HMAC CHECK");
 
 slack_payload = var('request.content');
 echo("slack_payload: " + slack_payload);
-
-//sp_user_name = var('request.form.user_name');
-//echo("sp_user_name: " + sp_user_name);
-
-//sp_channel_id = var('request.form.channel_id');
-//echo("sp_channel_id: " + sp_channel_id);
-
-//sp_text = var('request.form.text');
-//echo("sp_text: " + sp_text);
-
-//sp_token = var('request.form.token');
-//echo("sp_token: " + sp_token);
-
-//sp_response_url = var('request.form.response_url');
-//echo("sp_response_url: " + sp_response_url);
 
 x_slack_request_timestamp = var('request.header.x-slack-request-timestamp');
 echo("x_slack_request_timestamp: " + to_string(x_slack_request_timestamp));
@@ -53,9 +38,9 @@ if (local_slack_signature != x_slack_signature) {
   error('ERROR Invalid HMAC Signature')
 }
 
-// Make blank array
-// TS format: '2021-08-11T05:08:56Z'
-//.date_format('YYYY-MM-DDThh:mmZ')
+// Make blank array then array_push request form params
+// TS format needed by PROdb: iso8601
+// Append .date_format('YYYY-MM-DDThh:mmZ') to force a format, but simple now works in our case
 prodb_status_array = [];
 array_push(prodb_status_array, [
   'Slack Timestamp': to_date('now'),
@@ -78,7 +63,6 @@ array_push(prodb_status_array, [
 
 echo("TRANSFORM ARRAY TO JSON AND PUSH TO PRODB");
 // echo json to be sent to prodb status table
-// dump(transformed);
 prodb_status_array_json = json_encode(prodb_status_array);
 echo(prodb_status_array_json);
 
@@ -91,6 +75,7 @@ prodb_status_response = request(
    'Authorization: bearer '+ prodb_token
   ]
 )
+// This goes back to slack
 if (prodb_status_response['status'] = 200) {
 	respond('/iam Successfully Loaded to PROdb STATUSES Table', 200);
 }
