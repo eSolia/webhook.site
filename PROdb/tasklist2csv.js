6aa52b493b2763751ce033f2e096b209d0d28d5a
Rick Cogley <rick.cogley@esolia.co.jp>
2021-09-13 06:24:31 +0000

    Add check for expired url date

--- PROdb/tasklist2csv.js
+++ PROdb/tasklist2csv.js
@@ -31,33 +31,21 @@ if (!tokenisvalid) {
   error('Invalid token')
 }
 
+// How Epoch is Calculated in PROdb:
+// ToText(Format(ToDays(((Today() + Days(7))-ToDate("1970/01/01")))*86400,"#"))
+// The received epoch (now + 7d when email sent) much still be greater than current
+urlisvalid = to_number(epoch) > to_number('now'.date_format('X'));
+echo(urlisvalid);
+// If the URL was invalid return an error
+if (!urlisvalid) {
+  error('Expired URL');
+}
+
+
 if (hash_str == fcsj_token) {
 	echo("oh baby");
 }
 
-
-
-//1631491200
-//echo(base64_encode(epoch));
-//echo(bin2hex(epoch));
-//echo(bin2hex(hash_str));
-//dece = bin2hex(base64_decode(epoch));
-//echo(dece);
-
-//echo(bin2hex(hash(epoch, "md5")));
-//echo(hash(hash_str, "md5"));
-//echo("bin2hex of hash of hash_str: " + bin2hex(hash(hash_str, "md5")));
-
-//echo('1631404800'.hash('md5'));
-//echo("constant md5 hash: " + '1631491200'.hash('md5'));
-//echo("base64 of epoch md5: " + base64_encode(epoch.hash('md5')));
-//echo("base64 of hash md5: " + bin2hex(hash_str.hash('md5')));
-
-//echo('now'.date_format('x'))
-//echo(now());
-
-
-// since you can't unhash md5, probably obscure the date, then unobscure it, and do math to find out if it's ok
 
 // Get reference loads qty for comparison after load
 //echo("GET reference loads qty");
